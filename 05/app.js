912036fa3fb51cdfdc21067d67e5f547be860452
mohsen <mohsen.amirkhani@live.com>
2022-10-15 19:48:47 +0330

    add extra comments

--- 05/app.js
+++ 05/app.js
@@ -15,6 +15,7 @@ app.use("/add-product", (req, res, next) => {
   res.send("<form action='/product' method='POST'><input type='text' name='title'><button>Add Product</button></form>"); // send the response
 });
 
+//another difference of get vs use is that 'get' would match the url with the exact provided path
 //app.get , app.post , app.put and app.delete are the other forms of app.use but only triggers for that specific method
 app.post('/product', (req, res) => {
     const body = req.body;
